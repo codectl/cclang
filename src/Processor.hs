56ff8962cb09bd8a6b8fcecd1ab7f70bfefae8fe
Renato Teixeira <rena2damas@gmail.com>
2018-01-03 23:41:51 +0100

    Added logic

--- src/Processor.hs
+++ src/Processor.hs
@@ -18,7 +18,17 @@ type Program = ([Macro], State, [Token])
 
 -- Applies token to the current state resulting in a different state
 evalToken :: [Macro] -> State -> Token -> State
-evalToken macros state token = state
+evalToken macros (vars, Stack stack, out) token
+  | elem token $ words "+ - * / % < > = !! && ||" = case pop $ Stack stack of
+    (Nothing, _)             -> error "peeking empty stack"
+    (Just top1, Stack stack1) -> case pop $ Stack stack1
+                                  (Nothing, _)               -> error "peeking empty stack"
+                                  (Just top2, Stack stack2) -> (vars, push (token top2 top1) $ Stack stack2, out) -- operator case
+  | token !! 0 == '@' = case pop $ Stack stack of
+    (Nothing, _)             -> error "peeking empty stack"
+    (Just top, Stack stack') -> (setVar vars (['@'] ++ token) top, Stack stack', out) -- variable case
+--  | token !! 0 == '$' = (vars)                                                      -- macro case
+  | otherwise = (vars, push (read token :: Integer) $ Stack stack, out)
 
 -- Computes the final state of a program from a starting point state
 eval :: Program -> State
@@ -33,9 +43,7 @@ processData rawFileData = return $ eval (macros, initialState, tokens)
     initialState = ([], Stack [], "")
     macros = loadMacros numMacros (tail fileData)
     numMacros = read $ head fileData
-    loadMacros n (l:ls)
-      | n == 0    = []
-      | otherwise = readMacro l : loadMacros (n-1) ls
+    loadMacros n (l:ls) = if n == 0 then [] else readMacro l : loadMacros (n-1) ls
     fileData = removeComments $ lines $ rawFileData
     removeComments fd = filter (\line -> head line /= '#') fd
 
@@ -53,23 +61,23 @@ readMacro line = readMacro' $ words line
     readMacro' ls = ((['$'] ++ head ls), (read $ head $ tail ls), (drop 2 ls))
 
 -- Get macro token by name
-getMacroTokens :: [Macro] -> Name -> [Token]
-getMacroTokens [] _ = error "Macro not found"
-getMacroTokens ((name, _, tokens):macros) lookup_name = if name == lookup_name then tokens else getMacroTokens macros lookup_name
+getMacroTokens :: [Macro] -> Name -> Maybe [Token]
+getMacroTokens [] _ = Nothing
+getMacroTokens ((name, _, tokens):macros) lookup_name = if name == lookup_name then Just tokens else getMacroTokens macros lookup_name
 
 -- Get macro arity by name
-getMacroArity :: [Macro] -> Name -> Arity
-getMacroArity [] _ = error "Macro not found"
-getMacroArity ((name, arity, _):macros) lookup_name = if name == lookup_name then arity else getMacroArity macros lookup_name
+getMacroArity :: [Macro] -> Name -> Maybe Arity
+getMacroArity [] _ = Nothing
+getMacroArity ((name, arity, _):macros) lookup_name = if name == lookup_name then Just arity else getMacroArity macros lookup_name
 
 -- Get variable value by name
-getVar :: [Variable] -> Name -> Value
-getVar [] _ = error "Variable not found"
-getVar ((name, value):variables) lookup_name = if name == lookup_name then value else getVar variables lookup_name
+getVar :: [Variable] -> Name -> Maybe Value
+getVar [] _ = Nothing
+getVar ((name, value):variables) lookup_name = if name == lookup_name then Just value else getVar variables lookup_name
 
 -- Set variable value by name
 setVar :: [Variable] -> Name -> Value -> [Variable]
-setVar [] _ _ = error "Variable not found"
+setVar [] name value = [(name, value)]
 setVar ((name, value):variables) lookup_name replace_value
   | name == lookup_name = (name, replace_value):variables
   | otherwise           = (name, value) : setVar variables lookup_name replace_value
